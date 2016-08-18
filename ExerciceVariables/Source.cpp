2919545aebc2d87fc31d05cc8352bc0906e3efa7
Donray Williams <w.donray@yahoo.com>
2016-08-18 13:07:37 -0500

    init

---
+++ ExerciceVariables/Source.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+int main()
+{
+	//EXAMPLE 1
+	//int num = 2/3 + 3/5;
+	//printf("%i", num);
+
+	//EXAMPLE 2
+	//int num = 2%3 + 3%5;
+	//printf("%i", num);
+
+
+	//EXAMPLE 3
+	//int num = 23 % 15 % -2;
+	//printf("%i", num);
+
+
+	//EXAMPLE 4
+	//int num = 25*1/2;
+	//printf("%i", num);
+
+
+	//EXAMPLE 5
+	//int num = 25*1.0/2;
+	//printf("%i", num);*/
+
+
+	//EXAMPLE 6
+	//int num = 25*(1/2);
+	//printf("%i", num);
+
+
+	//EXAMPLE 7
+	//int num = 235/8+7;
+	//printf("%i", num);
+
+
+	//EXAMPLE 8
+	//int num = 235/8.07;
+	//printf("%i", num);
+
+
+	//EXAMPLE 9
+	//int num = ((20+1) /2-2.0) /(23+3) *0.2;
+	//printf("%i", num);
+
+
+	//EXAMPLE 10
+	//Average Height
+
+
+	//EXAMPLE 11
+	//High Tax Rate
+
+
+	//EXAMPLE 12
+	//Low Tax Rate
+
+
+	//EXAMPLE 13
+	//Steam Libray Value
+
+	//EXAMPLE 14
+	//int a= 121
+
+
+	//EXAMPLE 15
+	//float b= 9.4f
+
+
+	//EXAMPLE 16
+	//char c= 'r'
+ 
+	//EXAMPLE 17
+	//int d= 100000
+
+	//EXAMPLE 18
+	//bool e= 0
+
+	//EXAMPLE 19
+	int a = 3;
+	int b = 4;
+	numa = a;
+		numb = b
+		printf("%i", numa);
+
+
+	system("pause");
+	return 0;
+
+}
