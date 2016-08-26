24d7c4861ed4a63b44d1b6b29d1313f5ec9a1981
Donray Williams <w.donray@yahoo.com>
2016-08-26 17:40:47 -0500

    Just practicing.

---
+++ Practice/Practice.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+void Variables();
+
+int main()
+{
+	Variables();
+	system("pause");
+	return 0;
+}
+
+void Variables()
+{
+	/*
+	1. Find the value of each of the following expressions, or explain why it is not a valid
+	expression.Try this without coding it first.
+	*/
+	std::cout << "-----------------------------------------" << std::endl;
+	std::cout << "			Problem : 1						" << std::endl;
+	std::cout << "-----------------------------------------" << std::endl;
+	std::cout << std::endl;
+	int a;								//Calculates each awnser 
+	a = 2 / 3 + 3 / 5;
+	std::cout << a << std::endl;
+	std::cout << std::endl;
+	int b;
+	b = 2 % 3 + 3 % 5;
+	std::cout << b << std::endl;
+	std::cout << std::endl;
+	int c;
+	c = 23 % 15 % -2;
+	std::cout << c << std::endl;
+	std::cout << std::endl;
+	int d;
+	d = 25 * 1 / 2;
+	std::cout << d << std::endl;
+	std::cout << std::endl;
+	float e;							//Used float to get awnser with decimal 
+	e = 25 * 1.0 / 2;
+	std::cout << e << std::endl;
+	std::cout << std::endl;
+	int f;
+	f = 25 * (1 / 2);
+	std::cout << f << std::endl;
+	std::cout << std::endl;
+	int g;
+	g = 235 / 8 + 7;
+	std::cout << g << std::endl;
+	std::cout << std::endl;
+	float h;							//Used float to get awnser with decimal 
+	h = 235 / 8.0 + 7;
+	std::cout << h << std::endl;
+	std::cout << std::endl;
+	float i;							//Used float to get awnser with decimal 
+	i = ((20 + 1) / 2 - 2.0) / (23 + 3)*0.2;
+	std::cout << i << std::endl;
+	std::cout << std::endl;
+	/*
+	2. Select suitable variable names you might use for:
+	*/
+	std::cout << "-----------------------------------------" << std::endl;
+	std::cout << "			Problem : 2 " << std::endl;
+	std::cout << "-----------------------------------------" << std::endl;
+	std::cout << std::endl;
+	std::cout << "A. avgHeight" << std::endl;     //a) The average height of students in your class
+	std::cout << std::endl;
+	std::cout << "B. hightTax" << std::endl;     //b) The tax rate for incomes over $65000
+	std::cout << std::endl;
+	std::cout << "C. lowTax" << std::endl;		//c) The tax rate for incomes below $5000
+	std::cout << std::endl;
+	std::cout << "D. steamTotal" << std::endl; //d) The total cost of all the games on your Steam library
+	std::cout << std::endl;
+	/*
+	3. Select suitable data types to hold the following values:
+	*/
+	std::cout << "-----------------------------------------" << std::endl;
+	std::cout << "			Problem : 3 " << std::endl;
+	std::cout << "-----------------------------------------" << std::endl;
+	std::cout << std::endl;
+
+
+}
