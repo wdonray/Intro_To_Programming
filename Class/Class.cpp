581f4a32e714229c3ee51acf0421fdc67ac3e643
Donray Williams <w.donray@yahoo.com>
2016-09-07 17:55:30 -0500

    Finished classes exercise.

--- Class/Class.cpp
+++ Class/Class.cpp
@@ -5,11 +5,24 @@ int main()
 {
 	Zombie first = Zombie(100, 20, "Dad");
 	Zombie second = Zombie(200, 30, "Boxer");
-	
 	for (int i = 0; i < 5; i++)
 	{
 		first.attack(second);
 	}
+
+	Inventory zombiefirstInv;
+	Inventory zombiesecondInv;
+
+	Item leg = Item( "Leg", 15, true, 4 );
+	Item arm = Item( "Arm", 10, true, 3 );
+	Item brain = Item( "Brain", 10, false, 1 );
+	
+	zombiefirstInv.addItem(leg);
+	zombiefirstInv.addItem(arm);
+	zombiefirstInv.printInventory();
+	zombiesecondInv.addItem(brain);
+	zombiesecondInv.printInventory();
+
 	system("pause");
 	return 0;
 }
--- Class/ProjectZom.h
+++ Class/ProjectZom.h
@@ -1,12 +1,74 @@
 #pragma once
+#include <string>
 class Zombie
 {
 public:
 	Zombie(int h, int d, const char * o);
-	void attack(Zombie &); 
+	void attack(Zombie &);
 
 private:
 	int m_hp; //declares a variable called m_hp with the type interger
 	int m_damage; //declares a variable called m_damage with type interger
-	std::string m_occupation; //
+	std::string m_occupation; //Declares a string called occupation
+};
+
+class Item
+{
+public:
+	Item() {};
+
+	Item(char * name, int weight, bool stackable, int stackcount) 
+	{
+		m_name = name; 
+		m_weight = weight;
+		m_stackable = stackable;
+		m_stackCount = stackcount;
+
+	}
+ char * m_name; // A name for each item
+private:
+	
+	float m_weight; // Declare a weight with type interger
+	bool m_stackable; // If you can stack or not 
+	int m_stackCount; // Max stack amount 
+};
+
+class Inventory
+{
+public:
+	Inventory()
+	{
+		m_count = 0;
+	};
+	bool addItem(Item&item)
+	{
+		/*for (int i = 0; i < 16; i++)
+		{
+			if (m_items[i] != NULL)
+			{
+				m_items[i] = item;
+				break;
+			}
+
+		}*/
+		if (m_count > 16)
+		{
+			return false;
+		}
+		m_items[m_count] = item;
+		m_count++;
+		return true;
+	}
+
+	void printInventory()
+	{
+		for (int i = 0; i < m_count; i++)
+		{
+			std::cout << "Item @ " << i << " is a " << m_items[i].m_name << "\n";
+		}
+	}
+private:
+	Item m_items[16];
+	int m_size = 16;
+	int m_count;
 };
