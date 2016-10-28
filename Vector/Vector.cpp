b57013b15e5759e86b209983b68ee30df20fecdf
Donray Williams <w.donray@yahoo.com>
2016-10-27 22:59:54 -0500

    Noticed errors when testing

--- Vector/Vector.cpp
+++ Vector/Vector.cpp
@@ -8,6 +8,10 @@ int main()
 {
 	Vector2D * A = new Vector2D(2, 5);
 	Vector2D B = Vector2D(7, 8);
+	Vector3D * Aa = new Vector3D(3, 5, 2.5);
+	Vector3D Bb = Vector3D(6, 9, 5);
+	//Aa->Normalize();
+	Aa->add(Bb);
 	A->add(B);
 	A->Subtract(B);
 	system("pause");
