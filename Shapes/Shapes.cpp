bf9368200ebdaa69f551624327dfe8e541e83019
Donray Williams <w.donray@yahoo.com>
2016-12-07 17:57:42 -0600

    test

--- Shapes/Shapes.cpp
+++ Shapes/Shapes.cpp
@@ -5,9 +5,9 @@ int main()
 {
 	Triangle test = Triangle(1, 2, 3, 4, 5, 6, "Red");
 	Circle test2 = Circle(1, 2, 3, "Blue");
-	Line test3 = Line(1,2,4,5,"Help");
-	test.DebugPrint();
-	test2.DebugPrint();
+	Line test3 = Line(3,1,3,1,"Help");
+	/*test.DebugPrint();
+	test2.DebugPrint();*/
 	test3.DebugPrint();
 	system("pause");
 	return 0;
